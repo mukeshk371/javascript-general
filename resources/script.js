77a7068a9afd66c5c9ebe36703d3713cca24005e
mukesh.kumar <mukesh.kumar@decimal.co.in>
2021-04-07 17:26:40 +0530

    added 6 questions

--- resources/script.js
+++ resources/script.js
@@ -146,3 +146,63 @@ function returnBooleanToStringConversion() {
     result = boolToString.toString();
     alert(result);
 }
+
+function calculateFuel() {
+    let inputDistance = Number(prompt('Enter Distance'));
+    let requiredFuel = inputDistance * 10;
+    if (inputDistance <= 10 && inputDistance > 0) {
+        alert('Fuel = 100L');
+    } else if (inputDistance <= 0) {
+        alert('No fuel required');
+    } else {
+        alert('Fuel = ' + requiredFuel + 'L');
+    }
+}
+
+function calculateFramesPerSecond(minutes, fps) {
+    minutes = Number(prompt('Enter time in minutes')) * 60;
+    fps = Number(prompt('Enter frames'));
+    let framesPerSecond = minutes * fps;
+    alert('Frames Per Second = ' + framesPerSecond);
+}
+
+function yearsInOneHouse(age, moves) {
+    age = Number(prompt('Enter age'));
+    moves = Number(prompt('Enter years'));
+    let yearsLived = (age + moves) / 2;
+    alert('average I have lived = ' + yearsLived);
+}
+
+function stringCalculator(str) {
+    str = eval(prompt('Enter Expression'));
+    alert(str);
+}
+function makesTen(a, b) {
+    a = Number(prompt('Enter first number'));
+    b = Number(prompt('Enter second number'));
+    if (a == 10 || b == 10 || (a + b) == 10) {
+        alert('Sum = ' + (a + b) + ' ' + true);
+    } else {
+        alert(false);
+    }
+}
+
+function greeting(name) {
+    name = prompt('Enter a name');
+    if (name == 'Mubashir') {
+        alert('Hello, my love!');
+    } else {
+        alert('Hello, ' + name + ' !');
+    }
+}
+function maxNum(firstNum, SecondNum) {
+    firstNum = Number(prompt('Enter First Number'));
+    SecondNum = Number(prompt('Enter Second Number'));
+    if (firstNum > SecondNum) {
+        alert('Largest Number = ' + firstNum);
+    } else if (firstNum == SecondNum) {
+        alert('Both are equal');
+    } else {
+        alert('Largest Number = ' + SecondNum);
+    }
+}
